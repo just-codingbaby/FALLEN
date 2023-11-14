2c3d6043091a1c3dcea4944d46c55fa6adde6d73
DaYeong <day0_ng@naver.com>
2023-11-15 00:18:33 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, .'
 author = '.'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------

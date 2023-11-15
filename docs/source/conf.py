31675428b1dabda9ff789a878545934840dc49b8
DaYeong <day0_ng@naver.com>
2023-11-16 02:50:46 +0900

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sphinx_rtd_theme
 
 project = 'FALLEN'
 copyright = '2023, .'
@@ -27,6 +28,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']

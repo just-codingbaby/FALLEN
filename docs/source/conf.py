96fd3a36c6e040cab02b183325d3f118b0e6e810
DaYeong <day0_ng@naver.com>
2023-11-16 01:01:14 +0900

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    "myst_parser"]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +26,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
